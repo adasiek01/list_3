1e449d9f6528e7c13a509b0eebc079321a5d5b1c
Adam Kawałko <262329@student.pwr.edu.pl>
2021-11-09 16:13:13 +0100

    better version

--- ex_1.py
+++ ex_1.py
@@ -1,9 +1,8 @@
-def p(n, k, i):
+def probability(n, k, p):
     def newton(a, b):
         score = 1
         for i in range(1, b + 1):
             score = score * (a - i + 1) / i
         return score
-    p = 1/2
-    return sum(i for i in range(0, k)) * newton(n, k) * p**i * (1-p)**(n-i)
+    return sum(i * newton(n, k) * p**i * (1-p)**(n-i) for i in range(0, k))
 
