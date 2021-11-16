e5e84a6528d1ac9472c549202f46ff2b57396526
Adam Kawałko <262329@student.pwr.edu.pl>
2021-11-16 21:19:10 +0100

    3

--- ex_3.py
+++ ex_3.py
@@ -1,4 +1,9 @@
 def counting_chars_without_ifs(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    lines = [line.replace(' ', '') for line in lines]
+    with open(filename, 'w') as f:
+        f.writelines(lines)
     file_ref = open(filename, 'r')
     text = file_ref.read()
     small_text = text.lower()
@@ -11,4 +16,3 @@ def counting_chars_without_ifs(filename):
 
 if __name__ == "__main__":
     print(counting_chars_without_ifs("L3_ZAD3_sample_text.txt"))
-
