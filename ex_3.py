75f5735881d4775c44132ae92c164bdf9defca05
Adam Kawałko <262329@student.pwr.edu.pl>
2021-11-16 20:48:59 +0100

    2

--- ex_3.py
+++ ex_3.py
@@ -1,12 +1,13 @@
-def counting(title):
-    o = open(title, "r")
-    text = o.read()
-    dict_1 = {}
+def counting_chars_without_ifs(filename):
+    file_ref = open(filename, 'r')
+    text = file_ref.read()
+    char_count = {}
     for letter in text:
+        letter.lower()
         amount = text.count(letter)
-        dict_1[letter] = amount
-    return dict_1
+        char_count[letter] = amount
+    return char_count
 
 
 if __name__ == "__main__":
-    print(counting("aaa.txt"))
+    print(counting_chars_without_ifs("L3_ZAD3_sample_text.txt"))
