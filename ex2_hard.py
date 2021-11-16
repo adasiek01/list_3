3efa53faaaf406bf9067816f296f61f4e24c1235
Adam Kawałko <262329@student.pwr.edu.pl>
2021-11-16 22:11:48 +0100

    des

--- ex2_hard.py
+++ ex2_hard.py
@@ -1,4 +1,10 @@
 def smart_polynomial_value_calc(coeff, arg):
+    """
+     Function that counts how many multiplications we do to calculate the value of a polynomial of degree n
+     :param coeff: list of coefficients of a polynomial (from the smallest to the biggest)
+     :param arg: argument of the faction
+     :return: value of the polynomial, amount of multiplications, amount of additions
+     """
     count_mult = 0
     count_add = 0
     if len(coeff) == 1:
@@ -16,5 +22,6 @@ def smart_polynomial_value_calc(coeff, arg):
             count_add += 1
     return value, count_mult, count_add
 
+
 if __name__ == "__main__":
-    print(smart_polynomial_value_calc([1,0,-4,3,1],2))
+    print(smart_polynomial_value_calc([1, 0, -4, 3, 1], 2))
