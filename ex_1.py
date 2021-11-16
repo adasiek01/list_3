814f232422970db54245d5859fff22809a12fce1
adasiek01 <81614226+adasiek01@users.noreply.github.com>
2021-11-16 23:12:52 +0100

    Delete ex_1.py

--- ex_1.py
+++
@@ -1,8 +0,0 @@
-def probability(n, k, p):
-    def newton(a, b):
-        score = 1
-        for i in range(1, b + 1):
-            score = score * (a - i + 1) / i
-        return score
-    return sum(i * newton(n, k) * p**i * (1-p)**(n-i) for i in range(0, k))
-
