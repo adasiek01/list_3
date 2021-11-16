05eb5a8151f68ee44c44c877e45d444717cd11c1
Karolina Wypych <262333@student.pwr.edu.pl>
2021-11-16 21:04:16 +0100

    Smalll

--- ex_3.py
+++ ex_3.py
@@ -1,13 +1,14 @@
 def counting_chars_without_ifs(filename):
     file_ref = open(filename, 'r')
     text = file_ref.read()
+    small_text = text.lower()
     char_count = {}
-    for letter in text:
-        letter.lower()
-        amount = text.count(letter)
+    for letter in small_text:
+        amount = small_text.count(letter)
         char_count[letter] = amount
     return char_count
 
 
 if __name__ == "__main__":
     print(counting_chars_without_ifs("L3_ZAD3_sample_text.txt"))
+
